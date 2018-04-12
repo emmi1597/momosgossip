6bce2649c6d3573d54a460fe44cd1b0f7fbbd4ed
emmi1597 <35738015+emmi1597@users.noreply.github.com>
2018-04-12 09:07:38 -0500

    Add files via upload

--- logear.php
+++ logear.php
@@ -3,7 +3,7 @@
 
   	if (!empty($_POST['user'])) {
     	$user = $_POST['user'];
-    	$pas = $_POST['psw'];
+    	$psw = $_POST['psw'];
     	$stmt = $pdo->prepare("SELECT * FROM signup WHERE name = :nombre");
 		$stmt->execute([
 			':nombre'=>$user
@@ -17,6 +17,7 @@
     	// echo $user.$pas;
   	} 
 
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -25,6 +26,8 @@
   <title>Momo's Gossip Example</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </script>
   <style>
   .fakeimg {
@@ -37,6 +40,18 @@
   .clear{
     clear: both;
   }
+  .fa-snapchat-ghost {
+  background: #fffc00;
+  color: white;
+  text-shadow: -1px 0 black, 0 1px black, 1px 0 black, 0 -1px black;
+    padding: 20px;
+    font-size: 30px;
+    width: 30px;
+    text-align: right;
+    text-decoration: none;
+    border-radius: 50%;
+}
+}
   </style>
 </head>
 <body style="background-color: #CD5C5C;">
@@ -56,12 +71,7 @@
       </button>
     
     </div>
-    <div class="collapse navbar-collapse" id="myNavbar">
-      <ul class="nav navbar-nav">
-       
-        
-      </ul>
-    </div>
+   
   </div>
 </nav>
 
@@ -71,13 +81,18 @@
     <div class="col-sm-4">
       <h2>About Me</h2>
       <textarea name="comentarios" rows="10" cols="40">Escribe aquí tus comentarios</textarea>
+  <form action="logear.php" method="post">
+<input type="submit" name="grabar" value ="Like">
+<input type="submit" name="borrar" value ="Comentar">
+<input type="submit" name="editar" value ="Compartir">
 
       <h3 align="right">Status</h3>
 
       <select name="love" class=right >
 
-<option>Soltera</option>
+<option>Single</option>
 <option>In Love</option>
+<option>In a Relationship</option>
 <option>Complicated</option>
 <option>Married</option>
 
@@ -93,18 +108,42 @@
       <h2 >Coment:</h2>
       
       <textarea name="comentarios" rows="10" cols="40">Escribe aquí tus comentarios</textarea>
-     
+     <form action="logear.php" method="post">
+<input type="submit" name="grabar" value ="Like">
+<input type="submit" name="borrar" value ="Comentar">
+<input type="submit" name="editar" value ="Compartir">
+
       <p></p>
       <br>
       <h2>The people you love goes in here!</h2>
       
       <textarea name="comentarios" rows="10" cols="40">Escribe aquí tus comentarios</textarea>
-      <p>.</p>
+      <form action="logear.php" method="post">
+<input type="submit" name="grabar" value ="Like">
+<input type="submit" name="borrar" value ="Comentar">
+<input type="submit" name="editar" value ="Compartir">
+
+
+<form action="logear.php" method="post">
+if($_POST[grabar]) { instrucciones }
+
+
+if($_POST[borrar]) { instrucciones }
+
+
+if($_POST[editar]) { instrucciones }
+
+
     </div>
   </div>
 </div>
 
+
+  <h3 align="center">Social Media</h3>
+  
 <div class="jumbotron text-center" style="margin-bottom:0">
+  <a href="#" class="fa fa-snapchat-ghost"></a>
+
   <p align="center">XOXO...</p>
 </div>
 
